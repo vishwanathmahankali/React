d4e0369eea5576cc47913e9ac264a36380d68091
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-01 17:20:29 +0530

    passing custom arguments to event functions

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -6,8 +6,9 @@ import TabButton from "./components/TabButton.jsx";
 
 function App() {
 
-  function handleSelect() {
-    console.log('Hello world - selected!');
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    console.log(selectedButton);
   }
   return (
     <div>
@@ -29,10 +30,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
           Dynamic Content
         </section>
