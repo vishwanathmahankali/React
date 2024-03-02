08fa78f156abda5575a2adb9a8ab9c7d6c2f1015
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-02 13:08:18 +0530

    forwarding props to wrapped elements

--- react-essentials/src/components/Examples.jsx
+++ react-essentials/src/components/Examples.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import TabButton from "./TabButton.jsx";
 import { EXAMPLES } from "../data.js";
-
+import Section from "./Section.jsx";
 
 export default function Examples() {
 
@@ -30,32 +30,31 @@ export default function Examples() {
     
 
     return (
-        <section id="examples">
+        <Section id="examples">
         <h2>Examples</h2>
         <menu>
           <TabButton 
             isSelected={selectedTopic === 'components'} 
-            onSelect={() => handleSelect('components')}>
+            onClick={() => handleSelect('components')}>
             Components
           </TabButton>
           <TabButton 
             isSelected={selectedTopic === 'jsx'} 
-            onSelect={() => handleSelect('jsx')}>
+            onClick={() => handleSelect('jsx')}>
             JSX
           </TabButton>
           <TabButton 
             isSelected={selectedTopic === 'props'} 
-            onSelect={() => handleSelect('props')}>
+            onClick={() => handleSelect('props')}>
             Props
           </TabButton>
           <TabButton 
             isSelected={selectedTopic === 'state'} 
-            onSelect={() => handleSelect('state')}>
+            onClick={() => handleSelect('state')}>
             State
-          </TabButton>  
-          
+          </TabButton>           
         </menu>
         {tabContent}          
-      </section>
+      </Section>
     );
 }
