bc782c2cbe374e711e952a9578282c4859ec830d
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-03 20:42:22 +0530

    adding newproject & input component

---
+++ management-app/src/components/Input.jsx
@@ -0,0 +1,10 @@
+export default function Input({ label, textarea, ...props }) {
+    return (
+        <p>
+            <label htmlFor="">
+                {label}
+            </label>
+            {textarea ? <textarea {...props}/> : <input {...props}/>}
+        </p>
+    );
+}
