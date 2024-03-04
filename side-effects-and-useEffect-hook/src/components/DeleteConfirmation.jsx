85b03cec2b0e91f72b8bb43ab68e3748239db34d
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-04 19:58:34 +0530

    useEffect cleanup function

--- side-effects-and-useEffect-hook/src/components/DeleteConfirmation.jsx
+++ side-effects-and-useEffect-hook/src/components/DeleteConfirmation.jsx
@@ -1,12 +1,28 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
   
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
+  useEffect(() => {
+    console.log('INTERVAL');
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+    return () => {
+      clearInterval(interval);
+    };
+    },
+    []
+  );
+
   useEffect(() => {
     console.log("Timer set")
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
     
     return () => {
       console.log("cleaning up timer");
@@ -26,6 +42,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER}/>
     </div>
   );
 }
