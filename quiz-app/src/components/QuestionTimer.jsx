97d9fd619bb965fdde6863f4335a181d7b866921
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-04 22:18:35 +0530

    adding question timers

---
+++ quiz-app/src/components/QuestionTimer.jsx
@@ -0,0 +1,22 @@
+import {useState, useEffect} from 'react';
+
+export default function QuestionTimer({timeout, onTimeout}) {
+    
+    const [remainingTime, setRemainingTime] = useState(timeout);
+
+    useEffect(() => {
+        setTimeout(onTimeout, timeout);
+    }, [timeout, onTimeout]);
+
+    // setTimeout(onTimeout, timeout);
+    
+    useEffect(() => {
+        setInterval( () => {
+            setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
+        }, 100);    
+    }, []);
+
+    return (
+        <progress id="question-time" max={timeout} value={remainingTime}/>
+    );
+}
