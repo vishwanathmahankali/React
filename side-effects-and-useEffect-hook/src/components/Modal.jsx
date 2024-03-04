3327dd7db56b6449c0f37113550162c1c3d7faeb
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-04 18:59:14 +0530

    adding handleStopRemovePlace to Modal component

--- side-effects-and-useEffect-hook/src/components/Modal.jsx
+++ side-effects-and-useEffect-hook/src/components/Modal.jsx
@@ -1,24 +1,23 @@
 import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
-
-function Modal({ open, children }) {
+ 
+function Modal({ open, children, onClose }) {
   const dialog = useRef();
-
+ 
   useEffect(() => {
-    if(open){
+    if (open) {
       dialog.current.showModal();
     } else {
       dialog.current.close();
     }
   }, [open]);
-
-
+ 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
+    <dialog className="modal" ref={dialog} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById('modal')
   );
 }
-
+ 
 export default Modal;
