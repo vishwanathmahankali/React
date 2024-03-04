8cde1e694f21eecad87cf11e391745aa67c25316
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-04 18:04:10 +0530

    using useEffect for handling some side effect

--- side-effects-and-useEffect-hook/src/App.jsx
+++ side-effects-and-useEffect-hook/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -10,15 +10,21 @@ import {sortPlacesByDistance} from './loc.js';
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
+  const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState([]);
 
-  navigator.geolocation.getCurrentPosition((position) => {
-    const sortedPlaces = sortPlacesByDistance(
-      AVAILABLE_PLACES, 
-      position.coords.latitude, 
-      position.coords.longitude
-    );
-  });
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES, 
+        position.coords.latitude, 
+        position.coords.longitude
+      );
+  
+      setAvailablePlaces(sortedPlaces);
+    });  
+  }, []);
+
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -72,7 +78,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="Sorting places by distance..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
