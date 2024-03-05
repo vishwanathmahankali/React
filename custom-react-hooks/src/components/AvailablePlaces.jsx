24c54747280b0ae0e656068dd234a54aafe60a12
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-05 21:07:05 +0530

    using custom hook in multiple components

--- custom-react-hooks/src/components/AvailablePlaces.jsx
+++ custom-react-hooks/src/components/AvailablePlaces.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
 import Error from './Error.jsx';
@@ -7,15 +6,21 @@ import { fetchAvailablePlaces } from '../http.js';
 import { useFetch } from '../hooks/useFetch.js';
 
 
-// navigator.geolocation.getCurrentPosition((position) => {
-//   const sortedPlaces = sortPlacesByDistance(
-//     places,
-//     position.coords.latitude,
-//     position.coords.longitude
-//   );
-//   setAvailablePlaces(sortedPlaces);
-//   setIsFetching(false);
-// });
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      
+      resolve(sortedPlaces);
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
 
@@ -23,8 +28,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
     isFetching, 
     error, 
     fetchedData: availablePlaces, 
-    setFetchedData: setAvailablePlaces 
-  } = useFetch(fetchAvailablePlaces, []);
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
     return <Error title="An error occurred!" message={error.message} />;
