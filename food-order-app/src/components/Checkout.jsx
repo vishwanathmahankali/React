b1790e5dcb69e83c9f7f6e466bebb7fda63d110f
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-06 23:27:03 +0530

    handling form submission & validation

--- food-order-app/src/components/Checkout.jsx
+++ food-order-app/src/components/Checkout.jsx
@@ -6,7 +6,6 @@ import CartContext from "../store/CartContext";
 import UserProgressContext from "../store/UserProgressContext.jsx";
 import Input from "./UI/Input.jsx";
 
-
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
@@ -20,24 +19,31 @@ export default function Checkout() {
     userProgressCtx.hideCheckout();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+  }
+
   return (
-    <Modal 
-        open={userProgressCtx.progress === 'checkout'} 
-        onClose={handleClose}>
-      <form>
+    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
         <Input label="Full Name" type="text" id="full-name" />
-        <Input label="E-Mail Address" type="email" id="email"/>
+        <Input label="E-Mail Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
-            <Input label="Postal Code" type="text" id="postal-code"/>
-            <Input label="City" type="text" id="city" />
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
         </div>
-        
+
         <p className="modal-actions">
-            <Button type="button" textOnly onClick={handleClose}>Close</Button>
-            <Button>Submit Order</Button>
+          <Button type="button" textOnly onClick={handleClose}>
+            Close
+          </Button>
+          <Button>Submit Order</Button>
         </p>
       </form>
     </Modal>
