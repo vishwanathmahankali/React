d8ab6d0d2a6fb418f85ea08a4630b3152d2db72c
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-06 21:02:27 +0530

    Opening the Cart in the Model via a New Context

--- food-order-app/src/App.jsx
+++ food-order-app/src/App.jsx
@@ -1,15 +1,19 @@
+
 import Header from "./components/Header.jsx";
 import Meals from "./components/Meals.jsx";
+import Cart from "./components/Cart.jsx";
 import { CartContextProvider } from "./store/CartContext.jsx";
+import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
 
 function App() {
   return (
-    <>
+    <UserProgressContextProvider>
       <CartContextProvider>
         <Header />
         <Meals />
+        <Cart />
       </CartContextProvider>
-    </>
+    </UserProgressContextProvider>
   );
 }
 
