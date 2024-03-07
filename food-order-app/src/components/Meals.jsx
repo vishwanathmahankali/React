0132cffaeaa84662af5a3c3b21a01156d065ac4f
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-07 11:02:27 +0530

    custom Http hook

--- food-order-app/src/components/Meals.jsx
+++ food-order-app/src/components/Meals.jsx
@@ -1,22 +1,23 @@
-import { useState, useEffect } from "react";
 import MealItem from "./MealItem.jsx";
+import useHttp from "../hooks/useHttp.js";
+
+const requestConfig = {}
 
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  
+  const {
+    data: loadedMeals, 
+    isLoading, 
+    error
+ } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
+ console.log(loadedMeals);
 
-      if (!response.ok) {
-      }
-
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-
-    fetchMeals();
-  }, []);
+ if(isLoading) {
+  return (
+    <p>Fetching meals...</p>
+  );
+ }
 
   return (
     <ul id="meals">
