2bec77af456cb08e4cbddfca324cecc8a98ef470
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-10 15:19:26 +0530

    image slider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import "./App.css";
+import ImageSlider from "./components/image-slider";
 // import Accordian from "./components/accordian";
 // import RandomColor from "./components/random-color";
-import StarRating from "./components/star-rating";
+// import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
-            {/* Accordian Account */}
-            {/* <Accordian /> */}
+      {/* Accordian Account */}
+      {/* <Accordian /> */}
 
-            {/* Random color component */}
-            {/* <RandomColor /> */}
+      {/* Random color component */}
+      {/* <RandomColor /> */}
 
-            {/* Star-rating component */}
-            <StarRating noOfStars={10}/>
+      {/* Star-rating component */}
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
