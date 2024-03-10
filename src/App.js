bc06ef9979d12b907d5368cb842c7f004b019113
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-10 16:08:23 +0530

    load more data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
-import ImageSlider from "./components/image-slider";
 // import Accordian from "./components/accordian";
 // import RandomColor from "./components/random-color";
 // import StarRating from "./components/star-rating";
+// import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
+
 
 function App() {
   return (
@@ -17,11 +19,14 @@ function App() {
       {/* <StarRating noOfStars={10}/> */}
 
       {/* Image slider component */}
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> */}
+
+      {/* load more products */}
+      <LoadMoreData />
     </div>
   );
 }
