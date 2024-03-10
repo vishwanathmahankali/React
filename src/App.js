337dc5ee85c031ac59a03495f97ff09d5b171af4
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-10 11:38:06 +0530

    Generate random color HEX or RGB

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-import Accordian from "./components/accordian";
+// import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
 
 function App() {
   return (
     <div className="App">
             {/* Accordian Account */}
-            <Accordian />
+            {/* <Accordian /> */}
+
+            {/* Random color component */}
+            <RandomColor />
     </div>
   );
 }
