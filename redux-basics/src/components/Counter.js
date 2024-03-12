2d3ed38999f42b5c28914ae0af8d1f704e03777c
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-12 12:52:49 +0530

    Redux with class based components

--- redux-basics/src/components/Counter.js
+++ redux-basics/src/components/Counter.js
@@ -16,7 +16,17 @@ const Counter = () => {
     dispatch({type: 'decrement'});
   }
 
-
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
 };
 
 export default Counter;
