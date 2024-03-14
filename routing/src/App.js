f4b5838221596ac060b83e9ac5e318f7159fba6b
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-14 21:47:14 +0530

    defining routes

--- routing/src/App.js
+++ routing/src/App.js
@@ -1,5 +1,25 @@
+// import { createRoutesFromElements, Route } from "react-router-dom";
+import { createBrowserRouter,  RouterProvider } from "react-router-dom";
+import HomePage from './pages/Home';
+import ProductsPage from "./pages/Products";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage/>} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
+
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage />},
+  {path: '/products', element: <ProductsPage />}
+]);
+
+// const router = createBrowserRouter(routeDefinitions);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
