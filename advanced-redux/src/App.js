02b630f2a7f221b8868eba8fcdaeee00eab774d1
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-14 17:17:18 +0530

    using useEffect with Redux

--- advanced-redux/src/App.js
+++ advanced-redux/src/App.js
@@ -1,3 +1,5 @@
+import {useEffect} from 'react';
+
 import {useSelector} from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -7,6 +9,14 @@ import Products from './components/Shop/Products';
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-29a5a-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
