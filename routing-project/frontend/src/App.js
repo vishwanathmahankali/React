38388191df349e339bcebceb9d63348421d4c641
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-15 19:48:18 +0530

    reusing actions via request methods

--- routing-project/frontend/src/App.js
+++ routing-project/frontend/src/App.js
@@ -5,11 +5,13 @@ import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction
 } from "./pages/EventDetail";
-import NewEventPage, {action as newEventAction} from "./pages/NewEvent";
+import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
+import {action as manipulateEventAction} from './components/EventForm';
+
 
 const router = createBrowserRouter([
   {
@@ -39,14 +41,15 @@ const router = createBrowserRouter([
               },
               { 
                 path: "edit", 
-                element: <EditEventPage /> 
+                element: <EditEventPage /> ,
+                action: manipulateEventAction
               },
             ],
           },
           { 
             path: "new", 
             element: <NewEventPage />,
-            action: newEventAction
+            action: manipulateEventAction
           },
         ],
       },
