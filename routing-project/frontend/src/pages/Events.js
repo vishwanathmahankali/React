b913109409c49aabd8ba5f3c9bbf104912f26904
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-15 17:02:48 +0530

    custom error handling

--- routing-project/frontend/src/pages/Events.js
+++ routing-project/frontend/src/pages/Events.js
@@ -1,28 +1,25 @@
-import {useLoaderData} from 'react-router-dom';
+import { useLoaderData } from "react-router-dom";
 
-import EventsList from '../components/EventsList';
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
+  const data = useLoaderData();
+  // if(data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+  const events = data.events;
 
-  const events = useLoaderData();
-
-  return (
-    <>
-      {<EventsList events={events} />}
-    </>
-  );
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
-
 export async function loader() {
-    const response = await fetch("http://localhost:8080/events");
-
-    if (!response.ok) {
-      // ...
-    } else {
-      const resData = await response.json();
-      return resData.events;
-    }
+  const response = await fetch("http://localhost:8080/events");
+  if (!response.ok) {
+    // return {isError: true, message: 'Could not fetch events.'};
+    throw new Response(JSON.stringify({message: 'Could not fetch events.'}), {status: 500});
+  } else {
+    return response;
+  }
 }
