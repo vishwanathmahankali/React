9980ebd6c5a0aff541d2ee65565ff08dcaaa3aaf
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-15 20:51:28 +0530

    Welcome Home Page

--- routing-project/frontend/src/pages/Home.js
+++ routing-project/frontend/src/pages/Home.js
@@ -1,6 +1,11 @@
-function HomePage() {
-    return <h1>HomePage</h1>
-}
+import PageContent from "../components/PageContent";
 
+function HomePage() {
+    return (
+        <PageContent title="Welcome!">
+            <p>Browse all our amazing events!</p>
+        </PageContent>
+    );
+}
 
 export default HomePage;
