9dfff4d1b3584adbb42674916662b9c79c023dea
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-15 11:52:28 +0530

    showing error pages

--- routing/src/App.js
+++ routing/src/App.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
 //     <Route path="/" element={<HomePage/>} />
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
