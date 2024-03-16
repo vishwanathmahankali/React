058ee0aab20f3e04e3789d7967ea704011d02564
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-16 15:08:53 +0530

    Dynamic query functions & query keys

--- react-query/src/util/http.js
+++ react-query/src/util/http.js
@@ -1,14 +1,20 @@
-export async function fetchEvents() {
-    const response = await fetch('http://localhost:3000/events');
+export async function fetchEvents(searchTerm) {
+  let url = "http://localhost:3000/events";
 
-    if (!response.ok) {
-      const error = new Error('An error occurred while fetching the events');
-      error.code = response.status;
-      error.info = await response.json();
-      throw error;
-    }
-
-    const { events } = await response.json();
-
-    return events;
+  if (searchTerm) {
+    url += "?search=" + searchTerm;
   }
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the events");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { events } = await response.json();
+
+  return events;
+}
